c5eccb945eea7f1035589a713e30bab47380beac
Ivan Sierra <sierraivan93@gmail.com>
2025-11-08 15:29:39 -0300

    Se creo el metodo crear de PacienteDao

---
+++ src/main/java/utn/trabajoPracticoIntegrador/dao/PacienteDao.java
@@ -0,0 +1,57 @@
+package utn.trabajoPracticoIntegrador.dao;
+
+import utn.trabajoPracticoIntegrador.entities.Paciente;
+
+import java.sql.*;
+import java.util.List;
+
+public class PacienteDao implements GenericDao<Paciente>{
+    @Override
+    public void crear(Paciente entidad) {
+        //Try with resources para preparar la conexion y cerrarla al terminar
+        try (Connection conn = utn.trabajoPracticoIntegrador.config.DatabaseConnection.getConnection()) {
+                //Crear y ejecutar consulta SQL con PreparedStatement
+                String sql = "INSERT INTO paciente (eliminado, nombre, apellido, dni, fechaNacimiento) VALUES ( ?, ?, ?, ?, ? )";
+                //Try para preparar el insert SQL
+                try (PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                    pstmt.setBoolean(1, false);
+                    pstmt.setString(2, entidad.getNombre());
+                    pstmt.setString(3, entidad.getApellido());
+                    pstmt.setString(4, entidad.getDni());
+                    pstmt.setDate(5, Date.valueOf(entidad.getFechaNacimiento()));
+                    pstmt.executeUpdate();
+
+                    //Si se genero una clave, la leemos y asignamos la entidad
+                    try (ResultSet result = pstmt.getGeneratedKeys()) {
+                        if (result.next()){
+                            Long id = result.getLong("id");
+                            entidad.setId(id);
+                        }
+                    }
+                }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error al crear paciente." , e);
+        }
+    }
+
+
+    @Override
+    public Paciente leer(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Paciente> leerTodos() {
+        return List.of();
+    }
+
+    @Override
+    public void actualizar(Paciente entidad) {
+
+    }
+
+    @Override
+    public void eliminar(long id) {
+
+    }
+}
