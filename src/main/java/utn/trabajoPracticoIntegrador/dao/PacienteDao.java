5ccb99cf575b9e2c71b67a817a73cf837b7ede5c
Ivan Sierra <sierraivan93@gmail.com>
2025-11-08 23:34:01 -0300

    Se creo el metodo leer de PacienteDao

--- src/main/java/utn/trabajoPracticoIntegrador/dao/PacienteDao.java
+++ src/main/java/utn/trabajoPracticoIntegrador/dao/PacienteDao.java
@@ -1,8 +1,10 @@
 package utn.trabajoPracticoIntegrador.dao;
 
+import utn.trabajoPracticoIntegrador.entities.HistoriaClinica;
 import utn.trabajoPracticoIntegrador.entities.Paciente;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.List;
 
 public class PacienteDao implements GenericDao<Paciente>{
@@ -37,7 +39,29 @@ public class PacienteDao implements GenericDao<Paciente>{
 
     @Override
     public Paciente leer(long id) {
-        return null;
+        //Try with resources para preparar la conexion y cerrarla al terminar
+        try(Connection conn = utn.trabajoPracticoIntegrador.config.DatabaseConnection.getConnection()){
+            //Crear y ejecutar consulta SQL con PreparedStatement
+            String sql = "SELECT * FROM paciente WHERE id = ?";
+            //Try para preparar el insert SQL
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)){
+                pstmt.setLong(1, id);
+                try(ResultSet result = pstmt.executeQuery()){
+                    if (result.next()){
+                            boolean eliminado = result.getBoolean("eliminado");
+                            String nombre = result.getString("nombre");
+                            String apellido = result.getString("apellido");
+                            String dni = result.getString("dni");
+                            LocalDate fechaNacimiento = result.getDate("fechaNacimiento").toLocalDate();
+                        return new Paciente(id, eliminado, nombre, apellido, dni, fechaNacimiento, null );
+                    }else {
+                        return null;
+                    }
+                }
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("Error al leer paciente.", e);
+        }
     }
 
     @Override
